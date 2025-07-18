e8b50f62a74918e98b05e7dc5ef34714bccde5a4
stunkcuf <mychal.bert@gmail.com>
2025-07-17 18:01:39 -0700

    Fix missing formatBytes template function causing template loading failure

--- main.go
+++ main.go
@@ -219,6 +219,18 @@ func init() {
 			}
 			return strings.Join(words, " ")
 		},
+		"formatBytes": func(bytes int64) string {
+			const unit = 1024
+			if bytes < unit {
+				return fmt.Sprintf("%d B", bytes)
+			}
+			div, exp := int64(unit), 0
+			for n := bytes / unit; n >= unit; n /= unit {
+				div *= unit
+				exp++
+			}
+			return fmt.Sprintf("%.1f %cB", float64(bytes)/float64(div), "KMGTPE"[exp])
+		},
 	}
 
 	var err error
